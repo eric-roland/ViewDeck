43ff6ae40467b4eac1522177909b77b166d9d889
Eric Roland <eric.roland@gmail.com>
2014-08-11 16:10:47 -0400

    Update IIViewDeckController.h
    adds ability to hide statusbar

--- ViewDeck/IIViewDeckController.h
+++ ViewDeck/IIViewDeckController.h
@@ -200,6 +200,7 @@ typedef void (^IIViewDeckControllerBounceBlock) (IIViewDeckController *controlle
 
 @property (nonatomic, strong) NSString *centerTapperAccessibilityLabel; // Voice over accessibility label for button to close side panel
 @property (nonatomic, strong) NSString *centerTapperAccessibilityHint;  // Voice over accessibility hint for button to close side panel
+@property (nonatomic) BOOL shouldHideStatusBar;
 
 - (id)initWithCenterViewController:(UIViewController*)centerController;
 - (id)initWithCenterViewController:(UIViewController*)centerController leftViewController:(UIViewController*)leftController;
